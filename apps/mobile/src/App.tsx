0feac23dc616cb3eb25431019e5a9e0869a1b289
Christian Valentim <christianvalentim99@gmail.com>
2024-09-08 19:32:01 -0300

    mobile:(passando os componentes para o arquivo principal)

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,11 +1,44 @@
-import { Text, View } from 'react-native'
-import { somar } from '@barba/core'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { UserTaster } from './data/contexts/UserContext'
+import { SchedulingProvider } from './data/contexts/SchedulingContext'
+import { NavigationContainer } from '@react-navigation/native'
+import Register from './screens/Register'
+import Main from './screens/Main'
+import Summary from './screens/Summary'
+
+const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>App {somar(32, 48)}</Text>
-    </View>
+    <UserTaster>
+      <SchedulingProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='Cadastro'
+              component={Register}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name='Principal'
+              component={Main}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name='Sumario'
+              component={Summary}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SchedulingProvider>
+    </UserTaster>
   )
 }
 
