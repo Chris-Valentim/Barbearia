9707fa204d70f5252b94692daa1e020949b67348
Christian Valentim <christianvalentim99@gmail.com>
2024-09-08 19:25:45 -0300

    mobile:(criação de um array de imagens, mostrando os profissionais)

---
+++ apps/mobile/src/data/constants/images.ts
@@ -0,0 +1,56 @@
+const images = {
+  professionals: [
+      {
+          id: 1,
+          image: require('../../../assets/professionals/profissional-1.jpg'),
+      },
+      {
+          id: 2,
+          image: require('../../../assets/professionals/profissional-2.jpg'),
+      },
+      {
+          id: 3,
+          image: require('../../../assets/professionals/profissional-3.jpg'),
+      },
+      {
+          id: 4,
+          image: require('../../../assets/professionals/profissional-4.jpg'),
+      },
+      {
+          id: 5,
+          image: require('../../../assets/professionals/profissional-5.jpg'),
+      },
+      {
+          id: 6,
+          image: require('../../../assets/professionals/profissional-6.jpg'),
+      },
+  ],
+  services: [
+      {
+          id: 1,
+          image: require('../../../assets/services/corte-de-cabelo.jpg'),
+      },
+      {
+          id: 2,
+          image: require('../../../assets/services/corte-de-barba.jpg'),
+      },
+      {
+          id: 3,
+          image: require('../../../assets/services/manicure-pedicure.jpg'),
+      },
+      {
+          id: 4,
+          image: require('../../../assets/services/combo.jpg'),
+      },
+      {
+          id: 5,
+          image: require('../../../assets/services/corte-infantil.jpg'),
+      },
+      {
+          id: 6,
+          image: require('../../../assets/services/dia-de-noivo.jpg'),
+      },
+  ],
+}
+
+export default images
