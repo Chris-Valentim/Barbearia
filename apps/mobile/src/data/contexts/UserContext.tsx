303b133e7bd636cef3686dbca75cb068e64351ea
Christian Valentim <christianvalentim99@gmail.com>
2024-09-08 19:23:44 -0300

    mobile:(criação dos componentes que serão usados no contexts)

---
+++ apps/mobile/src/data/contexts/UserContext.tsx
@@ -0,0 +1,60 @@
+'use client'
+import { createContext, useCallback, useEffect, useState } from 'react'
+import { User } from '@barba/core'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+export interface UserContextProps {
+  loading: boolean
+  user: User | null
+  singIn: (user: User) => Promise<void>
+  getOut: () => void
+}
+
+const UserContext = createContext<UserContextProps>({} as any)
+
+export const UserTaster = ({ children }: any) => {
+  const { get, set } = useLocalStorage()
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState<User | null>(null)
+
+  const userLoaded = useCallback(
+    function () {
+      try {
+        const userLocal = get('user')
+        if (userLocal) {
+          setUser(userLocal)
+        }
+      } finally {
+        setLoading(false)
+      }
+    },
+    [get]
+  )
+
+  async function singIn(user: User) {
+    setUser(user)
+    set('user', user)
+  }
+
+  function getOut() {
+    setUser(null)
+    set('user', null)
+  }
+
+  useEffect(() => userLoaded(), [userLoaded])
+
+  return (
+    <UserContext.Provider
+      value={{
+        loading,
+        user,
+        singIn,
+        getOut
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export default UserContext
